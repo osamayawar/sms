23b100296cbbd6cc4ece70a75b070c9d0808ac8e
Osama Yawar <osamayawar@hotmail.com>
2017-03-08 12:24:36 -0500

    Added seeder for users table

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('users')->delete();
+        User::create(array(
+            'name'     => 'Chris Sevilleja',
+            'email'    => 'chris@scotch.io',
+            'password' => Hash::make('awesome'),
+        ));
+    }
+}
