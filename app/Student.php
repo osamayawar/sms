59b970bd3d590ed2ee62c7840a36adc7cbd65cf7
Osama Yawar <osamayawar@hotmail.com>
2017-03-09 15:13:38 -0500

    Added fillable to Student model

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = ['firstname', 'lastname', 'email', 'address', 'contact', 'user_id'];
+
+    protected $hidden = ['remember_token'];
 }
