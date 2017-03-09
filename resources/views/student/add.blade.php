4fc608e90bfd2b90322c38230ddc2b27f20b7a6c
Osama Yawar <osamayawar@hotmail.com>
2017-03-09 15:14:01 -0500

    Add new student form added

--- resources/views/student/add.blade.php
+++ resources/views/student/add.blade.php
@@ -2,4 +2,29 @@
 
 @section('content')
 <h1> Add new student </h1>
+<form action="/student/add" method="POST">
+	{{ csrf_field() }}
+	<div class="form-group">
+    	<label>First Name</label>
+    	<input type="firstname" class="form-control" name="firstname" placeholder="First Name">
+  	</div>
+  	<div class="form-group">
+    	<label>Last Name</label>
+    	<input type="lastname" class="form-control" name="lastname" placeholder="Last Name">
+  	</div>
+  	<div class="form-group">
+    	<label>Email</label>
+    	<input type="email" class="form-control" name="email" placeholder="Email">
+  	</div>
+  	<div class="form-group">
+    	<label>Address</label>
+    	<input type="address" class="form-control" name="address" placeholder="Address">
+  	</div>
+  	<div class="form-group">
+    	<label>Contact</label>
+    	<input type="tel" class="form-control" name="contact" placeholder="Contact">
+  	</div>
+  	
+  	<button type="submit" class="btn btn-default">Submit</button>
+</form>
 @endsection
